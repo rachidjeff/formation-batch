836e334b91a71738f9ce93af975d6a01682eb230
rachid_jeffali <rachidj@protonmail.ch>
2021-04-09 01:16:37 +0200

    BatchConfig.java avec un JobPArametersValidator, un step et un job

--- formation-batch/src/main/java/com/rachidj/formationbatch/FormationBatchApplication.java
+++ formation-batch/src/main/java/com/rachidj/formationbatch/FormationBatchApplication.java
@@ -1,12 +1,9 @@
 package com.rachidj.formationbatch;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-@EnableBatchProcessing
 public class FormationBatchApplication {
 
 	public static void main(String[] args) {
