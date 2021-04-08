e73e46cb7ebe517c09aec9da59b24a7fa7478907
rachid_jeffali <rachidj@protonmail.ch>
2021-04-09 00:03:53 +0200

    initialisation du projet

---
+++ formation-batch/src/main/java/com/rachidj/formationbatch/FormationBatchApplication.java
@@ -0,0 +1,13 @@
+package com.rachidj.formationbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormationBatchApplication.class, args);
+	}
+
+}
---
+++ formation-batch/src/test/java/com/rachidj/formationbatch/FormationBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rachidj.formationbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormationBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
